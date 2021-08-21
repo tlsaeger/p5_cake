c44613aebd3cc874d0f576be2a5734a8948ab859
Tom Saeger <tlsaeger@icloud.com>
2021-08-21 13:21:25 +0200

    basic setup with moving cake

--- sketch.js
+++ sketch.js
@@ -1,41 +1,66 @@
 let img, video;
-let predictions = [];
+let facePredictions = [];
+let handPredictions = [];
 let facemesh;
-// let emojiData;
-let input,
-  emojiInput,
-  generateButton,
-  emojiSizeInput,
-  fontSelector,
-  fontSizeInput,
-  colorPicker,
-  emojiWheel;
-let emoji = "🧠";
-
+let handpose;
+let faceModelLoadedIndi = false;
+let handModelLoadedIndi = false;
+let cakeMover = 0;
 
 function setup() {
-createCanvas(200,200)
-video = CreateCapture(VIDEO);
+createCanvas(1080,720)
+video = createCapture(VIDEO);
+handpose = ml5.handpose(video, handModelLoaded);
+handpose.on('predict', results => {
+  handPredictions = results;
+})
+video.hide();
 }
-function draw(){}
+function draw(){
+  background(125)
+  image(video,0,0);
+  if (faceModelLoadedIndi && handModelLoadedIndi){
+    drawFaceKeypoints();
+    drawHandKeypoints();
+  }
+  textSize(48)
+  cakeMover += 0.1;
+  text('🎂',cakeMover, height/2)
+}
+function faceModelLoaded(){
+  console.log("Face")
+  faceModelLoadedIndi = true;
+}
+function handModelLoaded(){
+  console.log("Hand")
+  handModelLoadedIndi = true;
+  facemesh = ml5.facemesh(video, faceModelLoaded);
+  facemesh.on('predict', results => {
+  facePredictions = results;
+});
+}
 
-function drawKeypoints() {
-  for (let i = 0; i < predictions.length; i += 1) {
-    const keypoints = predictions[i].scaledMesh;
-    for (let j = 0; j < selectedPoints.length; j++) {
-      let [x, y] = keypoints[selectedPoints[j].pos];
-      push();
-      textSize(parseInt(emojiSizeInput.value()));
-      text(emoji, x, y);
-      pop();
-      push();
-      textSize(parseInt(fontSizeInput.value()));
-      if (selectedPoints[j].label.length > 15) {
-        let broken = selectedPoints[j].label.replace(/\s/, "\n");
-        text(broken, x, y - 80);
-      } else {
-        text(selectedPoints[j].label, x, y - 60);
-      }
+function drawFaceKeypoints() {
+  for (let i = 0; i < facePredictions.length; i += 1) {
+    const keypoints = facePredictions[i].scaledMesh;
+
+    // Draw facial keypoints.
+    for (let j = 0; j < keypoints.length; j += 1) {
+      const [x, y] = keypoints[j];
+
+      fill(0, 255, 0);
+      ellipse(x, y, 5, 5);
+    }
+  }
+}
+function drawHandKeypoints() {
+  for (let i = 0; i < handPredictions.length; i += 1) {
+    const prediction = handPredictions[i];
+    for (let j = 0; j < prediction.landmarks.length; j += 1) {
+      const keypoint = prediction.landmarks[j];
+      fill(0, 255, 0);
+      noStroke();
+      ellipse(keypoint[0], keypoint[1], 10, 10);
     }
   }
 }
